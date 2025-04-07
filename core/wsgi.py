0551a832eb324d6f7b3831a9ef7980ce345726a1
kubatbaew <kubatbaew@gmail.com>
2025-04-07 08:15:42 +0600

    add project

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,23 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
+# This file contains the WSGI configuration required to serve up your
+# web application at http://oshsudastan20250407.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Django project
 
 import os
+import sys
 
+# add your project directory to the sys.path
+project_home = '/home/oshsudastan20250407/analog_class_room'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'analog_class_room.settings'
+
+
+# serve django via WSGI
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = get_wsgi_application()
+
